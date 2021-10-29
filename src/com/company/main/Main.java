4734bc5549c47e1c9fc794e12c5201ee247c3bb5
M3L-N1K <93345052+M3L-N1K@users.noreply.github.com>
2021-10-29 17:13:11 +0300

    T-01
    ForStatementDemo

---
+++ src/com/company/main/Main.java
@@ -0,0 +1,12 @@
+package com.company.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int count ;
+
+        for (count = 0; count < 7; count = count + 1)
+        System.out.println("Переменная цикла равна " + count);
+        System.out.print("Цикл окончен");
+    }
+}
